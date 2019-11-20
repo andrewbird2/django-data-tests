3d9bcf58cc35603aeb728e4eff40f09b11d1c378
Andrew Bird <andrew.bird@vesparum.com>
2019-11-20 10:47:58 -0800

    Allow user to run one test from management command

--- data_tests/management/commands/rundatatests.py
+++ data_tests/management/commands/rundatatests.py
@@ -9,11 +9,26 @@ class Command(BaseCommand):
     help = "Run data tests"
 
     def add_arguments(self, parser):
-        parser.add_argument('model', nargs='?', default=None)
+        parser.add_argument(
+            "-m",
+            "--model",
+            dest="model",
+            default=None
+        )
+        parser.add_argument(
+            "-t",
+            "--test",
+            dest="test_method",
+            default=None
+        )
 
     def handle(self, *args, **options):
         model = options.get('model')
-        if model:
+        test_method = options.get('test_method')
+        if test_method:
+            tm = TestMethod.objects.get(method_name=test_method)
+            tm.run_test_method()
+        elif model:
             qs = ContentType.objects.filter(model__iexact=model)
             if qs.count() > 1:
                 raise Exception('More than one %s model exists in codebase' % model)
@@ -21,3 +36,4 @@ class Command(BaseCommand):
             TestMethod.rerun_tests_for_model(model_class)
         else:
             TestMethod.rerun_all_tests()
+

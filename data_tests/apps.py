d7ea00b913a407dc6d2897809b1360a79e1c70c6
Andrew Bird <andrew.bird@vesparum.com>
2019-12-06 09:01:48 +1100

    Do not populate tests on app load

--- data_tests/apps.py
+++ data_tests/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 
 class DataTestsConfig(AppConfig):
     name = 'data_tests'
-
-    def ready(self):
-        from .registry import populate_test_methods
-        populate_test_methods()

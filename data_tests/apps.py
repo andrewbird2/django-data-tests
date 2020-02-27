b3e49ff196e88e75a5beafcb2ba4852422ca11ae
Andrew Bird <andrew.bird@vesparum.com>
2020-02-27 14:16:40 +1100

    Ensure migrations are run before model tests are loaded

--- data_tests/apps.py
+++ data_tests/apps.py
@@ -1,11 +1,15 @@
-# -*- coding: utf-8
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
+
+def post_migration_callback(sender, **kwargs):
+    from data_tests.registry import load_test_methods
+    load_test_methods()
 
 class DataTestsConfig(AppConfig):
     name = 'data_tests'
     verbose_name = 'Data tests'
 
     def ready(self):
-        from data_tests.registry import load_test_methods
-        load_test_methods()
+        post_migrate.connect(post_migration_callback, sender=self)
+
